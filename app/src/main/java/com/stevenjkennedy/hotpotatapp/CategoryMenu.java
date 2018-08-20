fdd5d15b57af82bec8254ed69b7eb4027b529c59
stevenjkennedy <stevenjkennedy@gmail.com>
2018-08-19 20:14:25 -0400

    Randomizing category contents

--- app/src/main/java/com/stevenjkennedy/hotpotatapp/CategoryMenu.java
+++ app/src/main/java/com/stevenjkennedy/hotpotatapp/CategoryMenu.java
@@ -37,8 +37,24 @@ public class CategoryMenu extends AppCompatActivity {
             selectedCategory = (String[])allCategories.toArray()[rand.nextInt(allCategories.size())];
         }
         Intent intent = new Intent(this, CoopGame.class);
+        shuffleArray(selectedCategory);
         intent.putExtra(WORDS,selectedCategory);
         startActivity(intent);
+    }
+
+    // Implementing Fisher–Yates shuffle
+    private static void shuffleArray(String[] ar)
+    {
+        // If running on Java 6 or older, use `new Random()` on RHS here
+        Random rnd = new Random();
+        for (int i = ar.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            String a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
     }
 
     static {
--- app/src/main/java/com/stevenjkennedy/hotpotatapp/CoopGame.java
+++ app/src/main/java/com/stevenjkennedy/hotpotatapp/CoopGame.java
@@ -7,12 +7,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Timer;
+
 
 public class CoopGame extends AppCompatActivity {
 
     protected String[] _words;
     protected Integer _correct = 0;
     protected Integer _wordIndex = 0;
+    protected Timer _timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,10 @@ public class CoopGame extends AppCompatActivity {
 
         Intent intent = getIntent();
         _words = intent.getStringArrayExtra(CategoryMenu.WORDS);
+    }
+
+    public void start(){
+        //Start timer
     }
 
     public void nextWord(View view){
